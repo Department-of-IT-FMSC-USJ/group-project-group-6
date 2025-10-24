03e6b84a4dd3cc300a1be78b909a3d192c224f19
SILVA K.R.H <112220@mgt.sjp.ac.lk>
2025-10-24 21:38:34 +0530

    Add  header.php

---
+++ views/header.php
@@ -0,0 +1,33 @@
+    <footer>
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-section">
+                    <h3>EcoCollect Lanka</h3>
+                    <p>Leading the digital transformation of plastic waste management in Sri Lanka.</p>
+                </div>
+                <div class="footer-section">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a href="/views/index.php">Home</a></li>
+                        <li><a href="/views/how-it-works.php">How It Works</a></li>
+                        <li><a href="/views/about.php">About Us</a></li>
+                        <li><a href="/views/location.php">Find Locations</a></li>
+                    </ul>
+                </div>
+                <div class="footer-section">
+                    <h4>Contact</h4>
+                    <p>Email: info@ecocollect.lk</p>
+                    <p>Phone: +94 11 234 5678</p>
+                </div>
+                <div class="footer-section">
+                    <h4>Follow Us</h4>
+                    <p>Facebook | Twitter | Instagram</p>
+                </div>
+            </div>
+            <div class="footer-bottom">
+                <p>&copy; 2025 EcoCollect Lanka. All rights reserved.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
